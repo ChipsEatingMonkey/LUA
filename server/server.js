8b15ce43ee83aec7af1a278dd977662e3ad9481e
Gleichlucas <lucas.kunz94@gmail.com>
2022-05-04 12:29:30 +0200

    passing arguments on Serverconnection

--- server/server.js
+++ server/server.js
@@ -3,11 +3,18 @@ const {Turtle , OPcodes} = require('../core/turtle.js');
 const WebS = require("ws")
 const wss = new WebS.Server({port:8081})
 
-wss.on("connection",ws=>{
-    console.log("connection!")
-    console.log(" ws url is: ");
-    console.log(ws.url);
-    ws.on("message",msg=>{
+wss.on("connection", function connection(ws, req){
+
+    //https://myhost:8081?isFrontend=true&uid=9999
+    const parameters = url.parse(req.url, true);
+    ws.uid = parameters.query.uid;
+    ws.isFrontend = parameters.query.isFrontend;
+    console.log("connection with: " + ws.uid + " , is frontend: " + ws.isFrontend);
+    console.log ("types are:");
+    console.log(typeof(ws.uid));
+    console.log(typeof(ws.isFrontend));
+
+    ws.on("message", function (msg){
 
         let rawData = msg.toString();
         let OPcode = msg.slice(0,4);
